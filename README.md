8d4fbf4e1507d20e65d4ee623cfc07e6865971c0
Marcus Cooke <marcus.j.cooke@gmail.com>
2016-06-04 16:47:26 +0100

    Grunt watch.

---
+++ README.md
@@ -0,0 +1,13 @@
+#Teemo-Scout
+A League of Legends scouter.
+
+To run.
+
+1. git clone {ThisRepo}
+2. npm i
+3. Make config.json like below
+
+	{
+    "api_key": "yourApiKey"
+	}
+4. Run grunt to compile sass
---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	watch: {
+  	  sass: {
+  	    files: 'src/scss/**/*.scss',
+  	    tasks: ['sass'],
+  	    options: {
+  	      interrupt: true,
+  	    },
+  	  },
+  	},
+
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'public/stylesheets/main.css': 'src/scss/_main.scss'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+
+};
